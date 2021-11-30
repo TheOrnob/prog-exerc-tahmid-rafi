2db9848eb61ad2e04771c0f718e61e039a2f2d58
TheOrnob <rpos.ornob@gmail.com>
2021-11-30 19:32:37 +0600

    sorted

---
+++ chapter-1.1/2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("January\nFebruary\nMarch\nApril\nMay\nJune\nJuly\nAugust\nSeptember\nOctober\nNovember\nDecember");
+
+    return 0;
+}
---
+++ chapter-1.1/3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Name: Rahat Pervej Ornob Sheikh\n");
+    printf("Father's name: MD Kamal Sehikh\n");
+    printf("Mother's name: Ruksana Pervin\n");
+
+    return 0;
+}
