362530aaa3bfe0bfcd219816eca782216ff35e4f
TheOrnob <rpos.ornob@gmail.com>
2021-11-30 19:12:40 +0600

    added chapter-1.1 1.c

---
+++ chapter-1.1/1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Saturday\n");
+    printf("Sunday\n");
+    printf("Monday\n");
+    printf("Tuesday\n");
+    printf("Wednesday\n");
+    printf("Thursday\n");
+    printf("Friday\n");
+
+    return 0;
+}
