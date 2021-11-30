f76fe79bd6a859daec903adf68eaac20eb727183
TheOrnob <rpos.ornob@gmail.com>
2021-11-30 20:26:39 +0600

    added 7.c

---
+++ chapter-1.1/7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int days; //number of days
+
+    scanf("%d", &days); //inputs the number of days
+
+    printf("%d\n", days/7); //days/7 will get us whole week count
+
+    return 0;
+}
