c96632b9d4b0fb961a7b1e897b35d50a99fda4e6
TheOrnob <rpos.ornob@gmail.com>
2021-11-30 20:29:44 +0600

    added 8.c

---
+++ chapter-1.1/8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int W; //number of week = W
+
+    scanf("%d", &W); 
+
+    printf("%d\n", W*3); // prints total number of episode in W week/s.
+
+    return 0;
+}
