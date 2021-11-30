0c5264acd4554a0746a0daf53f95375ee46ebab3
TheOrnob <rpos.ornob@gmail.com>
2021-11-30 20:06:56 +0600

    fixed error

---
+++ chapter-1.1/4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int exam_count;
+
+    scanf("%d", &exam_count); //inputs number of total exams
+    
+    printf("%d\n", exam_count); //as pen and exam count would be the same
+
+    return 0;
+}
